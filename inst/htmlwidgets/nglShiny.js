9d2e0b4e005b3486f72d9a78764fd73b1176f811
TJGorrie <tyler.gorrie-stone@diamond.ac.uk>
2020-05-14 15:46:32 +0100

    more adjustment

--- inst/htmlwidgets/nglShiny.js
+++ inst/htmlwidgets/nglShiny.js
@@ -119,9 +119,13 @@ if(HTMLWidgets.shinyMode) Shiny.addCustomMessageHandler("setPDB2", function(mess
     var stringBlob = new Blob( [ pdb ], { type: 'text/plain'} );
     console.log("nglShiny setPDB2:");
     stage.loadFile(stringBlob, { ext: "pdb" }).then(function (comp) {
-      comp.addRepresentation("ball+stick"); //, {sele: "ATOM"}); // Only show what is in protein
-      comp.addRepresentation("ball+stick", {sele: "LIG", color: "tomato"}); // Only show what is in ligand
-      comp.addRepresentation("contact");
+      comp.addRepresentation("ball+stick", {sele: "not (water or ion)"}); //, {sele: "ATOM"}); // Only show what is in protein
+      comp.addRepresentation("ball+stick", 
+        {sele: "LIG", 
+         aspectRatio: 3//,
+          //color: "tomato",
+          }); // Only show what is in ligand
+      comp.addRepresentation("contact", {sele: "not (water or ion)"});
       comp.autoView("LIG");
       comp.setParameters({'clipNear':42, 'clipFar':100, 'clipDist': 10, 'fogNear':50, 'fogFar':62});      
     });
